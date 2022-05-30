1f9dd0400dc423a27fa8b40643368878afa1a77d
stefano472 <stefanopiotti472@gmail.com>
2022-05-30 18:56:50 +0200

    route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+// Rotta per l'autenticazione gestita giá da larvarel
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function() {
+        Route::get('/', 'HomeController@index')
+        ->name('home');
+    });
+
+Route::get('{any}', function(){
+    return view('guest.home');
+})->where('any', '.*');
