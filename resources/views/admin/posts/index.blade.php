b41cb2ba352caa4b51d60f3c4db62320075dfffe
stefano472 <stefanopiotti472@gmail.com>
2022-06-01 17:08:27 +0200

    posts views

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.dashboard')
+
+
+
+@section('content')
+    <h1>HEllo</h1>
+@endsection
---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1>HEllo</h1>
+@endsection
