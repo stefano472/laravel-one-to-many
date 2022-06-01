d8f6359ebf89da92a1ce5bffec94d35a8e0a0250
stefano472 <stefanopiotti472@gmail.com>
2022-06-01 17:28:17 +0200

    posts views

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,1 +1,55 @@
 @extends('layouts.dashboard')
+
+@section('content')
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                {{-- Title card  --}}
+                <div class="card">
+                    <div class="card-header">Edit a Post</div>
+
+                    <div class="card-body">
+                    <form action="{{route('admin.posts.update', $post)}}" method="POST">
+                        {{-- Token  --}}
+                        @csrf
+                        {{-- / Token  --}}
+
+                        {{-- title post --}}
+                        @method('PUT')
+                        <div class="form-group">
+                            <label for="title">Title:</label>
+                            <input type="text" name="title" class="form-control @error('title') is-invalid @enderror " placeholder="Post's title"
+
+                            >@error('title')
+                            <div class="invalid-feedback">
+                                {{$message}}
+                            </div>
+
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="content">Content:</label>
+                            <textarea type="text" name="content" class="form-control @error('content') is-invalid @enderror" placeholder="Post's content">
+
+                            </textarea>
+                            @error('content')
+                                    <div class="invalid-feedback">
+                                        {{$message}}
+                                    </div>
+                            @enderror
+                        </div>
+                        {{--/ content post --}}
+
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-info white" value="Edit Post">
+                        </div>
+                    </form>
+                </div>
+            </div>
+                    <a href="{{route('admin.posts.index')}}" class="btn btn-success"> Back</a>
+
+            </div>
+        </div>
+    </div>
+@endsection
