bfdc1eb5f75bc6ada62abd5498a248fa55cd8635
stefano472 <stefanopiotti472@gmail.com>
2022-06-02 14:10:13 +0200

    predisposizione table e foreign key

--- app/Category.php
+++ app/Category.php
@@ -8,4 +8,12 @@ class Category extends Model
 {
     //
     protected $table = 'categories';
+
+    // utilizzo funzione public non static xk dovró richiamarla solo 
+    // nell'istanza (ovvero quando avró giá creato la classe category
+    // che qua per ora é solo definita
+    public function posts() {
+        return $this->hasMany('App\Post');
+    }
+
 }
--- app/Post.php
+++ app/Post.php
@@ -10,7 +10,21 @@ class Post extends Model
 {
     //
     protected $guarded = [];
+    // posso definire i campi che andró a fillare col form, con il guarded 
+    // ed in quel caso li prende tutti oppure con il 
+    // fillable vado a specificare solo quelli di interesse
 
+    // protected $fillable = ['title', 'content', 'slug', 'category_id'];
+
+    // utilizzo funzione public non static xk dovró richiamarla solo 
+    // nell'istanza (ovvero quando avró giá creato la classe category
+    // che qua per ora é solo definita
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
+
+    // in questo caso utilizzo funz static perché volendo la potrei richiamare
+    // anche se non ho giá creato la classe
     public static function convertToSlug($title) {
         $slugPrefix = Str::slug($title);
         $slug = $slugPrefix;
